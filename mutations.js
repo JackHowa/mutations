46abd580206ccf13c8ce18666ca5873ae45cebcd
jackhowa <johncarverhoward@gmail.com>
2017-08-10 15:30:47 -0500

    sort alphabetically and deduping the arrays

--- mutations.js
+++ mutations.js
@@ -1,10 +1,22 @@
 
-function mutation(arr) {
-  var acceptableLetters = arr[0].toLowerCase();
-  var checkingLetters = arr[1].toLowerCase(); 
+function mutation(mutationsArray) {
+  // find each string in the array 
+  // convert to lowercase so no sorting or matching probs 
+  var acceptableLetters = mutationsArray[0].toLowerCase();
+  var checkingLetters = mutationsArray[1].toLowerCase();
 
-  return checkingLetters.includes(acceptableLetters);
+  // sort alphabetically to check order 
+  var sortedAcceptable = acceptableLetters.split("").sort();
+  var sortedChecking = checkingLetters.split("").sort();
 
+  // remove duplicates 
+
+
+  // n
+
+
+  return acceptableLetters.includes(checkingLetters);
+  // acceptable letter has to be in checking 
 }
 
 console.log(mutation(["hello", "hey"]));
